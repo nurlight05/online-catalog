8c8f6f6c563e83a6ebc0bb66aac60fca881517fe
nurlight05 <nurlight05@gmail.com>
2022-07-13 00:21:13 +0500

    Добавил логин и регистрацию.

--- employee/urls.py
+++ employee/urls.py
@@ -7,6 +7,9 @@ router.register(r'employees', views.EmployerViewSet)
 
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index-list'),
+    path('login/', views.loginUser, name='login'),
+    path('register/', views.registerUser, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
     path('employee/<int:pk>/', views.EmployeeDetailView.as_view(), name='employee-detail'),
     path('employee/<int:pk>/employees/', views.EmployeesListView.as_view(), name='employees-list'),
     path('employees/', include(router.urls)),
--- employee/views.py
+++ employee/views.py
@@ -1,10 +1,66 @@
 from django.views.generic import DetailView, ListView
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.db.models import Q
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from rest_framework import viewsets
 
 from .serializers import EmployerSerializer
 from .models import Employer
+
+def loginUser(request):
+    page = 'login'
+    
+    if request.user.is_authenticated:
+        return redirect('index-list')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist!')
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('index-list')
+        else:
+            messages.error(request, 'Username or Password does not exist!') 
+            
+    context = {
+        'page': page
+    }
+    return render(request, 'employee/login_register.html', context)
+
+def registerUser(request):
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('index-list')
+        else:
+            messages.error(request, 'An error occured during registration!')
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'employee/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index-list')
 
 class IndexListView(ListView):
     template_name = 'employee/index.html'
